318879f11dfe93ffa2939df308fbbaa1b65dc244
Ivett Latak <doleance@gmail.com>
2018-07-11 20:04:19 +0200

    (BedMaking)NothingHere_JustSetingThingsUp

---
+++ src/main/java/com/greenfoxacademy/chat/DoleanceP2pApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoleanceP2pApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoleanceP2pApplication.class, args);
+    }
+}
